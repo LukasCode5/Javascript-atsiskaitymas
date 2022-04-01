5ab91a57985eada64842309d2073aa96b515f5f4
Lukasgit config --global user.email lukas@gmail.comgit add .git init -m init <you@example.com>
2022-04-01 16:01:09 +0300

    task 8 done.

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,32 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator() {
+  this.sum = function (number1, number2) {
+    return number1 + number2;
+  };
+  this.subtraction = function (number1, number2) {
+    return number1 - number2;
+  };
+  this.multiplication = function (number1, number2) {
+    return number1 * number2;
+  };
+  this.division = function (number1, number2) {
+    return number1 / number2;
+  };
+}
+
+const calculator1 = new Calculator();
+
+const calSum = calculator1.sum(80, 20);
+console.log("calSum", calSum);
+
+const calSubtraction = calculator1.subtraction(80, 20);
+console.log("calSubtraction", calSubtraction);
+
+const calMultiplication = calculator1.multiplication(80, 20);
+console.log("calMultiplication", calMultiplication);
+
+const calDivision = calculator1.division(80, 20);
+console.log("calDivision", calDivision);
